2e9132f1ec1f08a9ac090f4e73306d5303046a82
china-larry <china_larry@163.com>
2017-07-07 16:36:12 +0800

    donor details

--- DetectorPage/CDetectorPage.cpp
+++ DetectorPage/CDetectorPage.cpp
@@ -10,9 +10,9 @@ CDetectorPage::CDetectorPage(QWidget *parent) : QWidget(parent)
        _LoadQss();
 
     QGridLayout *grid = new QGridLayout;
-        grid->addWidget(createFirstExclusiveGroup(), 0, 0);
-        grid->addWidget(createSecondExclusiveGroup(), 1, 0);
-        grid->addWidget(createNonExclusiveGroup(), 0, 1);
+        grid->addWidget(_CreateDonorDetailsGroup(), 0, 0);
+        grid->addWidget(_CreateProductDetailsGroup(), 1, 0);
+        grid->addWidget(_CreateResultsGroup(), 0, 1);
         grid->addWidget(createPushButtonGroup(), 1, 1);
         setLayout(grid);
 
@@ -31,29 +31,83 @@ void CDetectorPage::_LoadQss()
    file.close();
 }
 
-QGroupBox *CDetectorPage::createFirstExclusiveGroup()
+QGroupBox *CDetectorPage::_CreateDonorDetailsGroup()
 {
-    QGroupBox *groupBox = new QGroupBox(tr("Exclusive Radio Buttons"));
+    QGroupBox *groupBox = new QGroupBox(tr("Donor Details"));
+    groupBox->setMaximumWidth(500);
+    // donor name
+    m_pDonorNameLabel = new QLabel(tr("Donor Name"), this);
+    m_pTemperatureCBox = new QCheckBox(tr("Temperature normal#"), this);
+    // last first donor
+    m_pLastNameLabel = new QLabel(tr("Last"), this);
+    m_pFirstNameLabel = new QLabel(tr("First"), this);
+    m_pDoorIDLabel = new QLabel(tr("Donor ID#"), this);
     //
-    //
+    m_pLastNameLineEdit = new QLineEdit(this);
+    m_pFirstNameLineEdit = new QLineEdit(this);
+    m_pDonorNameLineEdit = new QLineEdit(this);
+    // date of birth email
+    m_pDateofBirthLabel = new QLabel(tr("Date of Birth"), this);
+    m_pEmailAddressLabel = new QLabel(tr("Email Address"), this);
+    m_pBirthDateEdit = new QDateEdit(this);
+    m_pEmailAddressLineEdit = new QLineEdit(this);
+    // test time
+    m_pTestTimeLabel = new QLabel(tr("Test Time"), this);
+    m_pTestingSiteLabel = new QLabel(tr("Testing Site"), this);
+    m_pTestTimeEdit = new QDateTimeEdit(this);
+    m_pTestingSiteLineEdit = new QLineEdit(this);
+    // reason of test
+    m_pReasonfoTestLabel = new QLabel(tr("Reason for Test"), this);
+    m_pPreEmploymentCBox = new QCheckBox(tr("Pre Employment"), this);
+    m_pRandomCBox = new QCheckBox(tr("Random"), this);
+    m_pReasonableCBox = new QCheckBox(tr("Reasonable suspicion cause"), this);
+    m_pPostAccidentCBox = new QCheckBox(tr("PostAccident"), this);
+    m_pReturntoDutyCBox = new QCheckBox(tr("Return to Duty"), this);
+    m_pFollowUpCBox = new QCheckBox(tr("Follow Up"), this);
+    m_pOtherCBox = new QCheckBox(tr("Other: "), this);
+    m_pOtherLineEdit = new QLineEdit(this);
 
-        QRadioButton *radio1 = new QRadioButton(tr("&Radio button 1"));
-        QRadioButton *radio2 = new QRadioButton(tr("R&adio button 2"));
-        QRadioButton *radio3 = new QRadioButton(tr("Ra&dio button 3"));
 
-        radio1->setChecked(true);
+    QGridLayout *pGridLayout = new QGridLayout;
+    // donor name
+    pGridLayout->addWidget(m_pDonorNameLabel, 0, 0, 1, 2);
+    pGridLayout->addWidget(m_pTemperatureCBox , 0, 2, 1, 2);
+    // last fire name
+    pGridLayout->addWidget(m_pLastNameLabel, 1, 0, 1, 1);
+    pGridLayout->addWidget(m_pFirstNameLabel, 1, 1, 1, 1);
+    pGridLayout->addWidget(m_pDoorIDLabel, 1, 2, 1, 2);
 
-        QVBoxLayout *vbox = new QVBoxLayout;
-            vbox->addWidget(radio1);
-            vbox->addWidget(radio2);
-            vbox->addWidget(radio3);
-            vbox->addStretch(1);
-            groupBox->setLayout(vbox);
+    pGridLayout->addWidget(m_pLastNameLineEdit, 2, 0, 1, 1);
+    pGridLayout->addWidget(m_pFirstNameLineEdit, 2, 1, 1, 1);
+    pGridLayout->addWidget(m_pDonorNameLineEdit, 2, 2, 1, 2);
+    // date of birth email
+    pGridLayout->addWidget(m_pDateofBirthLabel, 3, 0, 1, 2);
+    pGridLayout->addWidget(m_pEmailAddressLabel, 3, 2, 1, 2);
+    pGridLayout->addWidget(m_pBirthDateEdit, 4, 0, 1, 2);
+    pGridLayout->addWidget(m_pEmailAddressLineEdit, 4, 2, 1, 2);
+    // test time
+    pGridLayout->addWidget(m_pTestTimeLabel, 5, 0, 1, 2);
+    pGridLayout->addWidget(m_pTestingSiteLabel, 5, 2, 1, 2);
+    pGridLayout->addWidget(m_pTestTimeEdit, 6, 0, 1, 2);
+    pGridLayout->addWidget(m_pTestingSiteLineEdit, 6, 2, 1, 2);
+    // reson of test
+    pGridLayout->addWidget(m_pReasonfoTestLabel, 7, 0, 1, 4);
+    pGridLayout->addWidget(m_pPreEmploymentCBox, 8, 0, 1, 2);
+    pGridLayout->addWidget(m_pRandomCBox, 8, 2, 1, 2);
+    pGridLayout->addWidget(m_pReasonableCBox, 9, 0, 1, 2);
+    pGridLayout->addWidget(m_pPostAccidentCBox, 9, 2, 1, 2);
+    pGridLayout->addWidget(m_pReturntoDutyCBox, 10, 0, 1, 2);
+    pGridLayout->addWidget(m_pFollowUpCBox, 10, 2, 1, 2);
+    pGridLayout->addWidget(m_pOtherCBox, 11, 0, 1, 1);
+    pGridLayout->addWidget(m_pOtherLineEdit, 11, 1, 1, 3);
+
+
+            groupBox->setLayout(pGridLayout);
 
             return groupBox;
 }
 
-QGroupBox *CDetectorPage::createSecondExclusiveGroup()
+QGroupBox *CDetectorPage::_CreateProductDetailsGroup()
 {
     QGroupBox *groupBox = new QGroupBox(tr("E&xclusive Radio Buttons"));
         groupBox->setCheckable(true);
@@ -77,7 +131,7 @@ QGroupBox *CDetectorPage::createSecondExclusiveGroup()
                 return groupBox;
 }
 
-QGroupBox *CDetectorPage::createNonExclusiveGroup()
+QGroupBox *CDetectorPage::_CreateResultsGroup()
 {
     QGroupBox *groupBox = new QGroupBox(tr("Non-Exclusive Checkboxes"));
         groupBox->setFlat(true);
--- DetectorPage/CDetectorPage.h
+++ DetectorPage/CDetectorPage.h
@@ -8,6 +8,9 @@
 #include <QCheckBox>
 #include <QRadioButton>
 #include <QMenu>
+#include <QLineEdit>
+#include <QDateEdit>
+#include <QDateTimeEdit>
 class CDetectorPage : public QWidget
 {
     Q_OBJECT
@@ -21,13 +24,46 @@ public slots:
 private:
     void _LoadQss();
 private:
-    QGroupBox *createFirstExclusiveGroup();
-    QGroupBox *createSecondExclusiveGroup();
-    QGroupBox *createNonExclusiveGroup();
+    QGroupBox *_CreateDonorDetailsGroup();
+    QGroupBox *_CreateProductDetailsGroup();
+    QGroupBox *_CreateResultsGroup();
     QGroupBox *createPushButtonGroup();
 
 private:
-    QLabel *m_pLabel;
+    // donor details 1line
+    QLabel *m_pDonorNameLabel;
+    QCheckBox *m_pTemperatureCBox;
+    // last first donor id
+    QLabel *m_pLastNameLabel;
+    QLabel *m_pFirstNameLabel;
+    QLabel *m_pDoorIDLabel;
+    //
+    QLineEdit *m_pLastNameLineEdit;
+    QLineEdit *m_pFirstNameLineEdit;
+    QLineEdit *m_pDonorNameLineEdit;
+    // data of birth email
+    QLabel *m_pDateofBirthLabel;
+    QLabel *m_pEmailAddressLabel;
+    //
+    QDateEdit *m_pBirthDateEdit;
+    QLineEdit *m_pEmailAddressLineEdit;
+    // test time
+    QLabel *m_pTestTimeLabel;
+    QLabel *m_pTestingSiteLabel;
+    //
+    QDateTimeEdit *m_pTestTimeEdit;
+    QLineEdit *m_pTestingSiteLineEdit;
+    // reason of test
+    QLabel *m_pReasonfoTestLabel;
+    QCheckBox *m_pPreEmploymentCBox;
+    QCheckBox *m_pRandomCBox;
+    QCheckBox *m_pReasonableCBox;
+    QCheckBox *m_pPostAccidentCBox;
+    QCheckBox *m_pReturntoDutyCBox;
+    QCheckBox *m_pFollowUpCBox;
+    QCheckBox *m_pOtherCBox;
+    QLineEdit *m_pOtherLineEdit;
+    // Product Details
 };
 
 #endif // CDETECTORPAGE_H
